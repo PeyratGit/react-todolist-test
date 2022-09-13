2fb788011b4adf99f5c66054c0185f0f13800cac
clement peyrat <aiglefeu31@gmail.com>
2022-09-13 12:48:23 +0200

    Coded Form and Header

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Form = () => {
+const Form = ({ todos, setTodos }) => {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const handleDescription = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, { id: todos.length + 1, title: title, description: description, isDone: false }]);
+    setTitle("");
+    setDescription("");
+  }
+
   return (
-    <div>Form</div>
+    <form onSubmit={formSubmit}>
+      <input type="text" placeholder='Task title' className='form-input' value={title} required onChange={handleTitle} />
+      <input type="text" placeholder='Task description' className='form-input' value={description} onChange={handleDescription} />
+      <button type='submit'>
+        Add
+      </button>
+    </form>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/todos">Todos</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header
