9737a80003827133ce687ccd855cb0fa1ce16fbe
clement peyrat <aiglefeu31@gmail.com>
2022-09-13 12:49:06 +0200

    Coded Todo page, displaying additional infos on the todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useLocation } from "react-router-dom";
 
 const Todo = () => {
+  const location = useLocation()
+  const { id, title, description, isDone } = location.state
   return (
-    <div>Todo</div>
+    <div className="todo-card">
+      <div className="todo-id">#{id}</div>
+      <h3>{title}</h3>
+      <div className="separator"></div>
+      <p>{description}</p>
+      <p className={`status ${isDone ? "done" : "not-done"}`}>{isDone ? "DONE" : "NOT DONE"}</p>
+    </div>
   )
 }
 
